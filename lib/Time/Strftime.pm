89a5897b3b8a2deaa6fc7d6616618aa9b4c110e7
tokuhirom <tokuhirom@gmail.com>
2013-03-01 21:21:11 +0900

    initial import

---
+++ lib/Time/Strftime.pm
@@ -0,0 +1,41 @@
+package Time::Strftime;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Time::Strftime - ...
+
+=head1 SYNOPSIS
+
+  use Time::Strftime;
+
+=head1 DESCRIPTION
+
+Time::Strftime is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
